a2eabe47396e8f79e605453cce4e9229a6fd0723
Ulrik Jorgensen <ulrik.jorgensen@openstudio.no>
2026-02-28 16:56:42 +0100

    Refactor Select component to include custom styling and toggleable dropdown.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,62 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+      <Wrapper>
+          <NativeSelect value={value} onChange={onChange}>
+              {children}
+          </NativeSelect>
+          <CustomSelect>
+              {displayedValue}
+              <IconWrapper style={{ '--size': 24 + 'px' }}>
+                  <Icon id="chevron-down" size={24}/>
+              </IconWrapper>
+          </CustomSelect>
+      </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+    position: relative;
+    width: max-content;
+    isolation: isolate;
+`;
+
+const NativeSelect = styled.select`
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    appearance: none;
+`
+
+const CustomSelect = styled.div`
+    font-size: 16px;
+    color: ${COLORS.gray700}; 
+    background-color: ${COLORS.transparentGray15};
+    padding: 12px 52px 12px 16px;
+    border-radius: 8px;
+    
+    ${NativeSelect}:focus + & {
+        outline: 2px auto -webkit-focus-ring-color;
+    }
+
+    ${NativeSelect}:hover + & {
+       color: ${COLORS.black}; 
+    }
+`;
+
+const IconWrapper = styled.div`
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    bottom: 0;
+    right: 10px;   
+    height: var(--size);
+    width: var(--size);
+    margin: auto;
+`
 
 export default Select;
